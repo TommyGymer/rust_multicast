c6d8f4fc7a6d3d1914fe5062a94c6b72274df516
Tom Gymer <tomgymer@proton.me>
2024-12-24 15:54:06 +0000

    remove hello world

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::{
 };
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
-
     let socket = UdpSocket::bind("[::1]:34000").expect("Couldn't bind to localhost:34000");
+    println!("Socket bound");
+
+    // socket.set_read_timeout(Some(Duration::new(1, 0))).unwrap();
 
     socket
         .join_multicast_v6(
@@ -16,18 +17,21 @@ fn main() -> Result<()> {
             0,
         )
         .expect("Unable to join multicast group");
+    println!("Joined multicast group");
 
     let multicast_addr =
-        SocketAddrV6::new(Ipv6Addr::new(65282, 0, 0, 0, 0, 0, 0, 256), 34000, 0, 0);
+        SocketAddrV6::new(Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0x100), 34000, 0, 0);
 
     loop {
         socket
             .send_to(b"testing", multicast_addr)
             .expect("Unable to send");
+        println!("Sent message");
 
         let mut buf = [0u8; 1400];
         let (mut number_of_bytes, mut src_addr) =
             socket.recv_from(&mut buf).expect("Unable to receive");
+        println!("{:?}", number_of_bytes);
         while number_of_bytes > 0 {
             let bytes = &buf[..number_of_bytes];
             println!("{:?}: {:?}", src_addr, bytes);
