a8be997b589e8ddfd0f271b1c10ae1b8709b9a5e
TommyGymer <84601129+TommyGymer@users.noreply.github.com>
2024-12-24 17:00:58 +0000

    split into threads

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use color_eyre::Result;
 use std::{
     net::{Ipv6Addr, SocketAddrV6, UdpSocket},
+    sync::Arc,
+    thread::sleep,
     time::Duration,
 };
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let multicast_addr =
         SocketAddrV6::new(Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0x100), 34000, 0, 0);
 
@@ -13,7 +16,7 @@ fn main() -> Result<()> {
     let socket = UdpSocket::bind(local).expect("Couldn't bind to localhost:34000");
     println!("Socket bound");
 
-    socket.set_read_timeout(Some(Duration::new(1, 0))).unwrap();
+    // socket.set_read_timeout(Some(Duration::new(1, 0))).unwrap();
 
     socket
         .join_multicast_v6(
@@ -27,15 +30,23 @@ fn main() -> Result<()> {
         .set_multicast_loop_v6(false)
         .expect("Unable to set multicast loop");
 
-    loop {
-        socket
-            .send_to(b"testing", multicast_addr)
-            .expect("Unable to send");
-        println!("Sent message");
+    let send_socket = Arc::new(socket);
+    let receive_socket = send_socket.clone();
 
+    tokio::spawn(async move {
+        loop {
+            send_socket
+                .send_to(b"testing", multicast_addr)
+                .expect("Unable to send");
+            println!("Sent message");
+            sleep(Duration::new(1, 0));
+        }
+    });
+
+    loop {
         let mut buf = [0u8; 1400];
 
-        while let Ok((number_of_bytes, src_addr)) = socket.recv_from(&mut buf) {
+        while let Ok((number_of_bytes, src_addr)) = receive_socket.recv_from(&mut buf) {
             let bytes = &buf[..number_of_bytes];
             println!("{:?}: {:?}", src_addr, String::from_utf8(bytes.to_vec()));
         }
